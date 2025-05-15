1efe7e79023de745a6ecf706c3963f7a9898e8e5
verycoolpotato <arandmagson@gmail.com>
2025-05-15 14:52:37 -0700

    Created Basic Camera Rig
    Camera now scrolls based on mouse position

--- enta-1233-2025-summer-main/1233_Template/Assets/1233_GameProgramming/ExternalPackages/Starter Assets/Runtime/ThirdPersonController/Scripts/CameraMotionTest.cs
+++ enta-1233-2025-summer-main/1233_Template/Assets/1233_GameProgramming/ExternalPackages/Starter Assets/Runtime/ThirdPersonController/Scripts/CameraMotionTest.cs
@@ -1,21 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Codice.CM.Common.Tree;
 using UnityEngine;
 
 public class CameraMotionTest : MonoBehaviour
 {
-   
-    bool aimState;
-    Vector3 screenCenter;
-
-    private void Awake()
-    {
-       
-    }
+    float yRotate;
+    float yPosition;
+    [SerializeField] float sensitivity;
     private void Update()
     {
-        
-        
+        Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2);
+
+        if ((Input.mousePosition - screenCenter).y < -120f)
+        {
+            yRotate -= 8 * Time.deltaTime * sensitivity;
+            yPosition -= 4 * Time.deltaTime * sensitivity;
+        }
+        else if((Input.mousePosition - screenCenter).y > 120f)
+        { 
+            yRotate += 8 * Time.deltaTime * sensitivity;
+            yPosition += 4 * Time.deltaTime * sensitivity;
+        }
+       
+        yRotate = Mathf.Clamp(yRotate, -1.5f, 1);
+        yPosition = Mathf.Clamp(yPosition, 0.4f, 0.7f);
+
+        transform.LookAt(new Vector3(6, yRotate, 0.4f));
+
+        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
 
        
         
